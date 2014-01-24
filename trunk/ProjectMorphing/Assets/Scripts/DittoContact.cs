c62f202d79de3f579345002bba62861c36e0ee47
shift.phase@gmail.com <shift.phase@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-24 16:51:10 +0000

    + ditto and logic picker

---
+++ trunk/ProjectMorphing/Assets/Scripts/DittoContact.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DittoContact : MonoBehaviour 
+{
+	private ModePicker modePicker;
+
+	void Start()
+	{
+		GameObject modePickerObject = GameObject.FindWithTag("ModeLogicPicker");
+		if(modePickerObject)
+		{
+			modePicker = modePickerObject.GetComponent<ModePicker>();
+		}
+		else
+		{
+			Debug.Log("Cannot find mode logic picker!");
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			// add to collect mode
+			modePicker.AddToCollector();
+		}
+		else if(other.tag == "PlayerProjectile")
+		{
+			// add to projectile mode
+			modePicker.AddToShooter();
+		}
+	}
+}
---
+++ trunk/ProjectMorphing/Assets/Scripts/ModePicker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModePicker : MonoBehaviour 
+{
+	private int shooter, collector, total;
+
+	void Update()
+	{
+		if(total == 2)
+		{
+
+		}
+	}
+
+	public void AddToShooter()
+	{
+		++shooter;
+		++total;
+	}
+
+	public void AddToCollector()
+	{
+		++collector;
+		++total;
+	}
+}
