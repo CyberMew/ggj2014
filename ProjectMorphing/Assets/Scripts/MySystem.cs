08c50e10e3a991df91ad12febdb6b0e1366caafb
treychua@gmail.com <treychua@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-24 23:00:51 +0000

    + it's ma systemz

---
+++ ProjectMorphing/Assets/Scripts/MySystem.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+class MySystem
+{
+	static public float sWIDTH = 1024f;
+	static public float sHEIGHT = 768f;
+
+	static Vector3 ScreenToWorld(float x, float y)
+	{
+		return Camera.main.ScreenToWorldPoint (new Vector3 (x, y, 0f));
+	}
+
+}
