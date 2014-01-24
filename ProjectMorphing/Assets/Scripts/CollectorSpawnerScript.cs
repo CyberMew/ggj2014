b0bfd5cebe6eaec2a63ad714cea3ec2cdbefdbaa
treychua@gmail.com <treychua@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-24 18:42:32 +0000

    + bunch of prefabs and scripts

---
+++ ProjectMorphing/Assets/Scripts/CollectorSpawnerScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectorSpawnerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// create obstacle
+	// create collectable
+
+	// obstacles remove themselves periodically
+	// calls for new obstacles to be created after delay X
+}
---
+++ ProjectMorphing/Assets/Scripts/WinLoseManagerScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinLoseManagerScript : MonoBehaviour {
+
+	public int objectsToSpawn = 200;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// reduce an object count
+	// works for either collecting collectables or killing enemies
+	void ReduceObjectCount()
+	{
+		--objectsToSpawn;
+
+		if (objectsToSpawn <= 0)
+			WinGame ();
+	}
+
+	// win the game
+	// show a display
+	void WinGame()
+	{
+	}
+
+	// called when the player is killed
+	// show a display
+	void LoseGame()
+	{
+	}
+
+}
