1da172340de3c82cc316947fc074659a1a203a2b
treychua@gmail.com <treychua@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-24 22:14:03 +0000

    + spawn scripts updated

--- ProjectMorphing/Assets/HybridSpawnScript.cs
+++ ProjectMorphing/Assets/HybridSpawnScript.cs
@@ -1,30 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ShooterSpawnScript : MonoBehaviour {
-	
+public class HybridSpawnScript : MonoBehaviour {
+
 	public GameObject[] enemyArray;
-	//public float[] enemySpawnChanceArray;
+
 	public GameObject[] buffArray;
 	public float[] buffSpawnChanceArray;
-
+	
 	public float initialBuffDelay;
 	private float delayBeforeBuffSpawns;
 
 	// Use this for initialization
 	void Start () {
 	
-		// check enemy spawn chances
-//		float totalPercentage = 0.0f;
-//		for (int i = 0; i < enemySpawnChanceArray.Length; ++i)
-//			totalPercentage += enemySpawnChanceArray[i];
-//		
-//		if(totalPercentage > 1.0f)
-//			Debug.Log ("ShooterSpawnScript::Start() - total combined percentage for enemy spawn chance exceeds 1!");
-//		else if(totalPercentage < 1.0f)
-//			Debug.Log ("ShooterSpawnScript::Start() - total combined percentage for enemy spawn chance is less than 1!");
-
-
 		// check buff spawn chances
 		float totalBuffPercentage = 0.0f;
 		for (int i = 0; i < buffSpawnChanceArray.Length; ++i)
@@ -34,18 +23,18 @@ public class ShooterSpawnScript : MonoBehaviour {
 			Debug.Log ("ShooterSpawnScript::Start() - total combined percentage for buff spawn chance exceeds 1!");
 		else if(totalBuffPercentage < 1.0f)
 			Debug.Log ("ShooterSpawnScript::Start() - total combined percentage for buff spawn chance is less than 1!");
-
-
+		
+		
 		// give an initial delay before buff spawns
 		delayBeforeBuffSpawns = initialBuffDelay;
-
+		
 		// start the buff spawning cycle
 		StartCoroutine (SpawnBuffIfApplicable ());
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 
 
@@ -53,7 +42,7 @@ public class ShooterSpawnScript : MonoBehaviour {
 	IEnumerator SpawnBuffIfApplicable()
 	{
 		yield return new WaitForSeconds (delayBeforeBuffSpawns);
-
+		
 		while (true)
 		{
 			// pick a buff randomly
@@ -66,33 +55,34 @@ public class ShooterSpawnScript : MonoBehaviour {
 				testValue += buffSpawnChanceArray[buffIndex];
 				++buffIndex;
 			}
-
+			
 			// spawn a new position for the buff that is not colliding with the player
 			// can be colliding with enemy
 			Vector2 spawnPosition = new Vector2(0,0);
 			bool isLegalPosition = false;
-
+			
 			while(!isLegalPosition)
 			{
 				// check for collision with player object //////////////////////////////////////////////////////////////////////////////////
 				isLegalPosition = true;
 			}
-
+			
 			// create the buff
 			Instantiate(buffArray[buffIndex], spawnPosition, Quaternion.identity);
-
+			
 			// generate new wait time
 			delayBeforeBuffSpawns = Random.Range (0.0f, delayBeforeBuffSpawns);
 			yield return new WaitForSeconds (delayBeforeBuffSpawns);
 		}
-
+		
 	}
+
 
 	// spawn an enemy based on the index given
 	void SpawnEnemy(int enemyIndex)
 	{
 		Vector2 spawnPosition = new Vector2(0,0);
-				
+		
 		// randomly find a position to spawn the collectable
 		// if collectable is colliding with anything, redo
 		bool isLegalPosition = false;
@@ -107,32 +97,4 @@ public class ShooterSpawnScript : MonoBehaviour {
 	}
 
 
-
-//	void SpawnEnemy()
-//	{
-//		Vector2 spawnPosition = new Vector2(0,0);
-//		
-//		// randomly select one of the enemies to spawn
-//		float randFloat = Random.Range (0.0f, 1.0f);
-//		float testValue = 0.0f;
-//		int spawnEnemyIndex = 0;
-//		
-//		while (testValue <= randFloat)
-//		{
-//			testValue += enemySpawnChanceArray[spawnEnemyIndex];
-//			++spawnEnemyIndex;
-//		}
-//
-//		// randomly find a position to spawn the collectable
-//		// if collectable is colliding with anything, redo
-//		bool isLegalPosition = false;
-//		while (!isLegalPosition) 
-//		{
-//			// check collision //////////////////////////////////////////////////////////////////////////////////////////////////////////
-//			isLegalPosition = true;
-//		}
-//		
-//		// spawn the object
-//		Instantiate (enemyArray [spawnEnemyIndex], spawnPosition, Quaternion.identity);
-//	}
 }
--- ProjectMorphing/Assets/Scripts/CollectorSpawnerScript.cs
+++ ProjectMorphing/Assets/Scripts/CollectorSpawnerScript.cs
@@ -3,11 +3,33 @@ using System.Collections;
 
 public class CollectorSpawnerScript : MonoBehaviour {
 	
-	public GameObject[] collectableArray;
+	public GameObject[] collectibleArray;
+	public float[] collectibleSpawnChanceArray;
+
 	public GameObject[] obstacleArray;
+	public float[] obstacleSpawnChanceArray;
 
 	// Use this for initialization
 	void Start () {
+
+		float totalPercentage = 0.0f;
+		for (int i = 0; i < collectibleSpawnChanceArray.Length; ++i)
+			totalPercentage += collectibleSpawnChanceArray [i];
+
+		if(totalPercentage > 1.0f)
+			Debug.Log ("CollectorSpawnScript::Start() - total combined percentage for collectible spawn chance exceeds 1!");
+		else if(totalPercentage < 1.0f)
+			Debug.Log ("CollectorSpawnScript::Start() - total combined percentage for collectible spawn chance is less than 1!");
+
+		float totalObstaclePercentage = 0.0f;
+		for (int i = 0; i < obstacleSpawnChanceArray.Length; ++i)
+			totalObstaclePercentage += obstacleSpawnChanceArray [i];
+		
+		if(totalObstaclePercentage > 1.0f)
+			Debug.Log ("CollectorSpawnScript::Start() - total combined percentage for obstacle spawn chance exceeds 1!");
+		else if(totalObstaclePercentage < 1.0f)
+			Debug.Log ("CollectorSpawnScript::Start() - total combined percentage for obstacle spawn chance is less than 1!");
+
 	}
 	
 	// Update is called once per frame
@@ -31,7 +53,15 @@ public class CollectorSpawnerScript : MonoBehaviour {
 		Vector2 spawnPosition = new Vector2(0,0);
 
 		// randomly select one of the collectables to spawn
-		int arrayPos = Random.Range (0, collectableArray.Length-1);
+		float randFloat = Random.Range (0.0f, 1.0f);
+		float testValue = 0.0f;
+		int index = 0;
+
+		while (testValue <= randFloat)
+		{
+			testValue += collectibleSpawnChanceArray[index];
+			++index;
+		}
 
 		// randomly find a position to spawn the collectable
 		// if collectable is colliding with anything, redo
@@ -44,6 +74,8 @@ public class CollectorSpawnerScript : MonoBehaviour {
 		}
 
 		// spawn the object
-		Instantiate (collectableArray [arrayPos], spawnPosition, Quaternion.identity);
+		Instantiate (collectibleArray [index], spawnPosition, Quaternion.identity);
 	}
+
+	
 }
