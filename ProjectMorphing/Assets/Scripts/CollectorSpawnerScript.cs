c9ff2e0d94d5a549b25697f5860064ae74749c5c
treychua@gmail.com <treychua@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-24 19:25:26 +0000

    + updated CollectorSpawnerScript, half completed, waiting for fabs for collectible game

--- ProjectMorphing/Assets/Scripts/CollectorSpawnerScript.cs
+++ ProjectMorphing/Assets/Scripts/CollectorSpawnerScript.cs
@@ -2,15 +2,16 @@
 using System.Collections;
 
 public class CollectorSpawnerScript : MonoBehaviour {
+	
+	public GameObject[] collectableArray;
+	public GameObject[] obstacleArray;
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
 
 	// create obstacle
@@ -18,4 +19,31 @@ public class CollectorSpawnerScript : MonoBehaviour {
 
 	// obstacles remove themselves periodically
 	// calls for new obstacles to be created after delay X
+	// change obstacle count every Y seconds
+
+	// collectables will spawn 1 by 1
+	// after a collectable gets taken, collectable will have a onDestroy that calls this function and 
+	// the winlose manager's ReduceObjectCount
+	// screenToWorldPoint
+	// 1024 x 768
+	void SpawnCollectable()
+	{
+		Vector2 spawnPosition;
+
+		// randomly select one of the collectables to spawn
+		int arrayPos = Random.Range (0, collectableArray.GetLength()-1);
+
+		// randomly find a position to spawn the collectable
+		// if collectable is colliding with anything, redo
+		bool isLegalPosition = false;
+		while (!isLegalPosition) 
+		{
+			// check collision
+
+			isLegalPosition = true;
+		}
+
+		// spawn the object
+		Instantiate (collectableArray [arrayPos], spawnPosition, Quaternion.identity);
+	}
 }
