7709e61f427bc42531d6b10773a43171d6f12121
shift.phase@gmail.com <shift.phase@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-24 22:34:22 +0000

    + many objects, so scripts, such prefabs, wow

---
+++ trunk/ProjectMorphing/Assets/Scripts/BasicCollectorScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicCollectorScript : MonoBehaviour 
+{
+	// this is the basic collect script.
+	// all collectibles will have this script
+	// which will call the body's add segment code
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			//other.gameObject.AddSegment();
+		}
+	}
+}
