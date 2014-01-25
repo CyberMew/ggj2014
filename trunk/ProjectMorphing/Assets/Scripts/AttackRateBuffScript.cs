5152298f38f2cad9808f881dbc319dad6a618a97
shift.phase@gmail.com <shift.phase@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-25 22:51:13 +0000

    + we are close i hope

---
+++ trunk/ProjectMorphing/Assets/Scripts/AttackRateBuffScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackRateBuffScript : MonoBehaviour 
+{
+	public float attackSpeedMod = 0.5f;
+	private AggressivePlayerAttack apa;
+	public float hasteDuration = 10f;
+	
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Player")
+		{
+			Destroy(gameObject);
+
+			apa = other.GetComponent<AggressivePlayerAttack>();
+			
+			if(apa)
+			{
+				apa.SetIsHasted(hasteDuration);
+				apa.cooldownBuffModifier = attackSpeedMod;
+			}
+			else
+			{
+				Debug.Log("HasteBuff | PlayerComponents not found!");
+			}
+		}
+	}
+}
---
+++ trunk/ProjectMorphing/Assets/Scripts/DestroyBodySegment.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyBodySegment : MonoBehaviour
+{
+	private PlayerBodyController pbc;
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Player")
+		{
+			Destroy(gameObject);
+			pbc = other.GetComponent<PlayerBodyController>();
+			if(pbc)
+			{
+				pbc.DestroyBodyAtEnd();
+			}
+			else
+			{
+				Debug.Log ("DestroyBodySegment | Collided with head: Cannot find body controller!");
+			}
+		}
+		else if(other.tag =="PlayerBody")
+		{
+			Destroy(gameObject);
+			pbc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBodyController>();
+			if(pbc)
+			{
+				pbc.DestroySpecificBody(other);
+			}
+			else
+			{
+				Debug.Log ("DestroyBodySegment | Collided with body: Cannot find body controller!");
+			}
+		}
+	}
+}
