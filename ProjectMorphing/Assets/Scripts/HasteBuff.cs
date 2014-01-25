fdd1a594df75ce3f78af3beb6af32fbcc44792c2
shift.phase@gmail.com <shift.phase@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-25 21:13:47 +0000

    ashata

---
+++ ProjectMorphing/Assets/Scripts/HasteBuff.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HasteBuff : MonoBehaviour 
+{
+	public float moveSpeedMod = 1.2f;
+	public float attackSpeedMod = 0.5f;
+	private AggressivePlayerMovement apm;
+	private AggressivePlayerAttack apa;
+	public float hasteDuration = 10f;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Player")
+		{
+			Destroy(gameObject);
+
+			apm = other.GetComponent<AggressivePlayerMovement>();
+			apa = other.GetComponent<AggressivePlayerAttack>();
+
+			if(apm && apa)
+			{
+				apm.moveSpeedBuffModifier = moveSpeedMod;
+				apm.SetIsHasted(hasteDuration);
+				apa.SetIsHasted(hasteDuration);
+				apa.cooldownBuffModifier = attackSpeedMod;
+			}
+			else
+			{
+				Debug.Log("HasteBuff | PlayerComponents not found!");
+			}
+		}
+	}
+}
