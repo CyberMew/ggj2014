a00efa05841ab424d7d5b86118f4f0d48474115b
whitekero87@gmail.com <whitekero87@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-25 22:25:55 +0000

    Fixed tag for AggressivePlayer
    Fixed collider radius size for Shield.prefab
    
    Fix shield "regen"

--- trunk/ProjectMorphing/Assets/Scripts/PlayerShield.cs
+++ trunk/ProjectMorphing/Assets/Scripts/PlayerShield.cs
@@ -3,21 +3,58 @@ using System.Collections;
 
 public class PlayerShield : MonoBehaviour 
 {
+	public float invulnerableTime = 3f;
+
+	private float curInvulTime = 0f;
 	private bool isShieldUp = true;
 	
 	void Start() {
 		gameObject.SetActive (isShieldUp);
+		curInvulTime = invulnerableTime;
 	}
 	
 	void Update() {
-		gameObject.SetActive (isShieldUp);
+		curInvulTime -= Time.deltaTime;
+		if(curInvulTime <= 0f && isShieldUp)
+		{
+			isShieldUp = false;
+			// Disable all components except transform or this
+			MonoBehaviour[] comps = GetComponents<MonoBehaviour>();
+			
+			foreach(MonoBehaviour c in comps)
+			{
+				c.enabled = false;
+			}
+			
+			this.enabled = true;
+			Debug.Log("Shields Disabled");
+		}
+		//gameObject.SetActive (isShieldUp);
+		//this.enabled = isShieldUp;
+		this.GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f, curInvulTime / invulnerableTime);
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
 	{
 		if(isShieldUp)
 		{
-			isShieldUp = false;
+			if(other.gameObject.tag == "EnemyProjectile")
+			{
+				Destroy (other.gameObject);
+			}
+		}
+	}
+
+	public void ShieldsUp() {
+		Debug.Log("Shields are up!");
+		isShieldUp = true;
+		curInvulTime = invulnerableTime;
+
+		MonoBehaviour[] comps = GetComponents<MonoBehaviour>();
+		
+		foreach(MonoBehaviour c in comps)
+		{
+			c.enabled = true;
 		}
 	}
 }
