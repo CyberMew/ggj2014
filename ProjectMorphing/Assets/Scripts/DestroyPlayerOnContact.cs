4a7e95ef8d5f35865219f71c6d6cc49294af6235
shift.phase@gmail.com <shift.phase@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-25 03:08:42 +0000

    asdddddddddddd

---
+++ ProjectMorphing/Assets/Scripts/DestroyPlayerOnContact.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// instagibs the player because players are noobz.
+
+public class DestroyPlayerOnContact : MonoBehaviour 
+{
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			Destroy(other.gameObject);
+		}
+	}
+}
