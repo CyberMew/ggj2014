19030baec481bf0ed64186abe80d846f233aad59
whitekero87@gmail.com <whitekero87@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-25 01:15:20 +0000

    Shields up!! Kamikaze!!

---
+++ ProjectMorphing/Assets/Scripts/EnemyShield.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShield : MonoBehaviour {
+
+	public GameObject shield;
+
+	private GameObject player;
+	private Vector3 dir;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		player = GameObject.FindGameObjectWithTag("Player");
+
+		if(player)
+		{
+			dir = player.transform.position - transform.position;
+			dir.Normalize ();
+		}
+
+		UpdateShield ();
+
+		if(transform.childCount == 0)
+		{
+			Invoke ("ShieldUp", 2f);
+		}
+	}
+
+	void ShieldUp() {
+		CancelInvoke ();
+
+		GameObject child = Instantiate (shield) as GameObject;
+		child.transform.parent = this.transform;
+		child.transform.localPosition = transform.position + dir;
+	}
+
+	void UpdateShield() {
+		if(transform.childCount != 0)
+			transform.GetChild (0).localPosition = transform.position + dir;
+	}
+}
---
+++ ProjectMorphing/Assets/Scripts/EnemySuicide.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySuicide : MonoBehaviour {
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		player = GameObject.FindGameObjectWithTag("Player");
+
+		Debug.Log(player);
+		if(player)
+		{
+			Vector3 dir = player.transform.position - transform.position;
+			dir.Normalize ();
+			transform.Translate (dir * Time.deltaTime);
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) 
+	{
+		//EnemyHealth script = GetComponent<EnemyHealth>();
+		//
+		//if(collision.gameObject.CompareTag("WallV") || collision.gameObject.CompareTag ("WallH") )
+		//{
+		//	gameObject.SendMessage("Hit", script.GetHP());
+		//	var particle = Instantiate(hitParticle, transform.position, transform.rotation);
+		//	Destroy (particle, 3);
+		//}
+		//else if(player == collision.gameObject)
+		//{			
+		//	gameObject.SendMessage("Hit", script.GetHP());
+		//	player.SendMessage ("Hit", damage);
+		//	var particle = Instantiate(hitParticle, transform.position, transform.rotation);
+		//	Destroy (particle, 3);
+		//}
+	}
+}
