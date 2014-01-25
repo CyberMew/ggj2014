81f7b17507b8f6c598a3856e0ee865673955cd49
treychua@gmail.com <treychua@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-25 02:29:02 +0000

    + my system

--- ProjectMorphing/Assets/Scripts/MySystem.cs
+++ ProjectMorphing/Assets/Scripts/MySystem.cs
@@ -6,6 +6,9 @@ class MySystem
 	static public float sWIDTH = 1024f;
 	static public float sHEIGHT = 768f;
 
+	static public Vector3 sWORLDDIMENSIONS = ScreenToWorldV3 (MySystem.sWIDTH, MySystem.sHEIGHT);
+	static public Vector3 sHALFWORLDDIMENSIONS = (ScreenToWorldV3 (MySystem.sWIDTH, MySystem.sHEIGHT) /= 2f);
+
 	static public Vector2 ScreenToWorldV2(float x, float y)
 	{
 		Vector3 v = ScreenToWorldV3(x, y);
