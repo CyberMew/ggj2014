5d0ed873d16ea94f63b92422d2f5f30b5e8aee9e
whitekero87@gmail.com <whitekero87@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-25 23:04:54 +0000

    + Screen wipe

---
+++ ProjectMorphing/Assets/Scripts/ExpandRingWipe.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExpandRingWipe : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, 1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localScale *= 1.2f;
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag != "Player")
+			Destroy (other.gameObject);
+	}
+}
