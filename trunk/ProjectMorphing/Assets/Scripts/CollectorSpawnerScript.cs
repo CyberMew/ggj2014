d6f92229107581c2277efb74eeec996930dae47e
treychua@gmail.com <treychua@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-25 20:02:14 +0000

    + obstacle spawning

--- trunk/ProjectMorphing/Assets/Scripts/CollectorSpawnerScript.cs
+++ trunk/ProjectMorphing/Assets/Scripts/CollectorSpawnerScript.cs
@@ -11,6 +11,7 @@ public class CollectorSpawnerScript : MonoBehaviour {
 
 	public float sqDistFromPlayer;
 	public float sqDistFromObstacle;
+	public float sqDistFromCollectible;
 
 	// Use this for initialization
 	void Start () {
@@ -33,6 +34,13 @@ public class CollectorSpawnerScript : MonoBehaviour {
 		else if(totalObstaclePercentage < 1.0f)
 			Debug.Log ("CollectorSpawnScript::Start() - total combined percentage for obstacle spawn chance is less than 1!");
 
+
+//		for (int i = 0; i < 50; ++i)
+//						SpawnCollectible ();
+
+		for (int i = 0; i < 30; ++i)
+			SpawnObstacle ();
+		
 	}
 
 	// create obstacle
@@ -41,11 +49,72 @@ public class CollectorSpawnerScript : MonoBehaviour {
 	// obstacles remove themselves periodically
 	// calls for new obstacles to be created after delay X
 	// change obstacle count every Y seconds
+	void SpawnObstacle()
+	{
+		Vector2 screenPosition = new Vector2(Random.Range(MySystem.edgeLeft,MySystem.edgeRight), Random.Range(MySystem.edgeBottom,MySystem.edgeTop));
+		Vector3 spawnPosition = new Vector3(screenPosition.x, screenPosition.y, 0f);
+		
+		// retrieve for checks
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		GameObject[] listOfCollectibles = GameObject.FindGameObjectsWithTag("Powerup");
+		
+		if (player == null)
+			Debug.LogError  ("CollectorSpawnerScript::SpawnObstacle - player can't be found!");
+		
+		// randomly find a position to spawn the collectable
+		// if collectable is colliding with player, redo
+		bool check = false;
+		
+		while (true) 
+		{
+			Vector3 distanceVector = spawnPosition - player.transform.position;
+			// first check against player
+			if(distanceVector.sqrMagnitude < sqDistFromPlayer)
+			{
+				spawnPosition = new Vector2(Random.Range(MySystem.edgeLeft,MySystem.edgeRight), Random.Range(MySystem.edgeBottom,MySystem.edgeTop));
+				continue;
+			}
+			
+			// second check against all obstacles
+			check = true;
+			for(int i = 0; i < listOfCollectibles.Length; ++i)
+			{
+				distanceVector = spawnPosition - listOfCollectibles[i].transform.position;
+				if(distanceVector.sqrMagnitude < sqDistFromCollectible){
+					check = false;
+					break;
+				}
+			}
+			
+			if(!check){
+				spawnPosition = new Vector2(Random.Range(MySystem.edgeLeft,MySystem.edgeRight), Random.Range(MySystem.edgeBottom,MySystem.edgeTop));
+				continue;
+			}
+			
+			break;
+		}
+		
+		// randomly pick the object and spawn it
+		float randFloat = Random.Range (0.0f, 1.0f);
+		int objIndex = 0;
+		float testValue = 0.0f;
+		
+		for(; objIndex < obstacleSpawnChanceArray.Length; ++objIndex)
+		{
+			testValue += obstacleSpawnChanceArray[objIndex];
+			
+			if(testValue >= randFloat)
+				break;
+		}
+		
+		MonoBehaviour.Instantiate (obstacleArray[objIndex], spawnPosition, Quaternion.identity);
+
+	}
 
 	// collectables will spawn 1 by 1
 	// after a collectable gets taken, collectable will have a onDestroy that calls this function and 
 	// the winlose manager's ReduceObjectCount
-	void SpawnCollectable()
+	void SpawnCollectible()
 	{
 		Vector2 screenPosition = new Vector2(Random.Range(MySystem.edgeLeft,MySystem.edgeRight), Random.Range(MySystem.edgeBottom,MySystem.edgeTop));
 		Vector3 spawnPosition = new Vector3(screenPosition.x, screenPosition.y, 0f);
@@ -55,7 +124,7 @@ public class CollectorSpawnerScript : MonoBehaviour {
 		GameObject[] listOfObstacles = GameObject.FindGameObjectsWithTag("Enemy");
 
 		if (player == null)
-			Debug.LogError  ("SpawnTemplate::SpawnObject - player can't be found!");
+			Debug.LogError  ("CollectorSpawnerScript::SpawnCollectable - player can't be found!");
 		
 		// randomly find a position to spawn the collectable
 		// if collectable is colliding with player, redo
