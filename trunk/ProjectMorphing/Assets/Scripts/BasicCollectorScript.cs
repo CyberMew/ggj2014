5483c9e32176926b758ae13c0893492a9a499c32
shift.phase@gmail.com <shift.phase@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-25 06:24:55 +0000

    + finally done phasing

--- trunk/ProjectMorphing/Assets/Scripts/BasicCollectorScript.cs
+++ trunk/ProjectMorphing/Assets/Scripts/BasicCollectorScript.cs
@@ -6,12 +6,47 @@ public class BasicCollectorScript : MonoBehaviour
 	// this is the basic collect script.
 	// all collectibles will have this script
 	// which will call the body's add segment code
+	private TimedPhasing timedPhasingComponent;
 
-	void OnTriggerEnter(Collider other)
+	void OnTriggerEnter2D(Collider2D other)
 	{
+		timedPhasingComponent = GetComponent<TimedPhasing>();
+
 		if(other.tag == "Player")
 		{
-			//other.gameObject.AddSegment();
+			if(timedPhasingComponent)
+			{
+				if(timedPhasingComponent.CanCollect())
+				{
+					Destroy(gameObject);
+					//other.gameObject.AddSegment();
+				}
+				else
+				{
+					timedPhasingComponent.TriggerPhaseOut();
+				}
+			}
+			else
+			{
+					Destroy(gameObject);
+					//other.gameObject.AddSegment();
+			}
+		}
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+		timedPhasingComponent = GetComponent<TimedPhasing>();
+		
+		if(other.tag == "Player")
+		{
+			if(timedPhasingComponent)
+			{
+				if(!timedPhasingComponent.CanCollect())
+				{
+					timedPhasingComponent.TriggerPhaseOut();
+				}
+			}
 		}
 	}
 }
