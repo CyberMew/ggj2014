44503d49af8f37b7dcf899000b28c4978f034424
cybermew@gmail.com <cybermew@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-25 18:38:38 +0000

    * Fixed bug for back button in Credits.unity
    + Added confirmation for main menu quit

--- ProjectMorphing/Assets/Scripts/MainMenuButton.cs
+++ ProjectMorphing/Assets/Scripts/MainMenuButton.cs
@@ -5,6 +5,7 @@ public class MainMenuButton : MonoBehaviour {
 
 	public string sceneName = "MainMenu";
 
+	private bool confirmQuit = false;
 	// Use this for initialization
 	void Start () {
 	
@@ -19,12 +20,38 @@ public class MainMenuButton : MonoBehaviour {
 	{
 		if(sceneName == "Exit" || sceneName == "Quit")
 		{
-			// todo: popup confirmation here
-			Application.Quit();
+			// Popup confirmation here
+			confirmQuit = true;
 		}
 		else
 		{
 			Application.LoadLevel(sceneName);
+		}
+	}
+
+	void OnGUI()
+	{
+		if(confirmQuit)
+		{
+			float centerX = Screen.width * 0.5f;
+			
+			Rect rect = new Rect(centerX, MySystem.sHEIGHT * 0.2f, 400f, 400f);
+			// Always Display normal buttons (Resume, Restart, Main Menu, Quit)
+			GUI.Box(rect, "This is your final chance. Do what you deem fit.");
+			
+			// Make all rect positions relative to the box's rect
+			GUI.BeginGroup(rect);
+			//	Start draw Resume button
+			if(GUI.Button(new Rect(0f, 0f, 100f, 20f), new GUIContent("Quit this now!")))
+			{
+				Application.Quit();
+			}
+			//	Start draw Restart button
+			if(GUI.Button(new Rect(0f, 40f, 100f, 20f), new GUIContent("Alright I'll stay.")))
+			{
+				confirmQuit = false;
+			}
+			GUI.EndGroup();
 		}
 	}
 }
