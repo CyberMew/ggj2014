31b400ab063bb339cd9a00f9d573786457fccb2c
treychua@gmail.com <treychua@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-25 06:02:28 +0000

    + more improvements to collector spawn manager

--- trunk/ProjectMorphing/Assets/Scripts/CollectorSpawnerScript.cs
+++ trunk/ProjectMorphing/Assets/Scripts/CollectorSpawnerScript.cs
@@ -10,6 +10,7 @@ public class CollectorSpawnerScript : MonoBehaviour {
 	public float[] obstacleSpawnChanceArray;
 
 	public float sqDistFromPlayer;
+	public float sqDistFromObstacle;
 
 	// Use this for initialization
 	void Start () {
@@ -46,8 +47,63 @@ public class CollectorSpawnerScript : MonoBehaviour {
 	// the winlose manager's ReduceObjectCount
 	void SpawnCollectable()
 	{
-		SpawnTemplate.SpawnObject (ref collectibleArray, ref collectibleSpawnChanceArray, sqDistFromPlayer);
-	}
+		Vector2 screenPosition = new Vector2(Random.Range(MySystem.edgeLeft,MySystem.edgeRight), Random.Range(MySystem.edgeBottom,MySystem.edgeTop));
+		Vector3 spawnPosition = new Vector3(screenPosition.x, screenPosition.y, 0f);
 
+		// retrieve for checks
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		GameObject[] listOfObstacles = GameObject.FindGameObjectsWithTag("Enemy");
+
+		if (player == null)
+			Debug.LogError  ("SpawnTemplate::SpawnObject - player can't be found!");
+		
+		// randomly find a position to spawn the collectable
+		// if collectable is colliding with player, redo
+		bool check = false;
+
+		while (true) 
+		{
+			Vector3 distanceVector = spawnPosition - player.transform.position;
+			// first check against player
+			if(distanceVector.sqrMagnitude < sqDistFromPlayer)
+			{
+				spawnPosition = new Vector2(Random.Range(MySystem.edgeLeft,MySystem.edgeRight), Random.Range(MySystem.edgeBottom,MySystem.edgeTop));
+				continue;
+			}
+
+			// second check against all obstacles
+			check = true;
+			for(int i = 0; i < listOfObstacles.Length; ++i)
+			{
+				distanceVector = spawnPosition - listOfObstacles[i].transform.position;
+				if(distanceVector.sqrMagnitude < sqDistFromObstacle){
+					check = false;
+					break;
+				}
+			}
+
+			if(!check){
+				spawnPosition = new Vector2(Random.Range(MySystem.edgeLeft,MySystem.edgeRight), Random.Range(MySystem.edgeBottom,MySystem.edgeTop));
+				continue;
+			}
+
+			break;
+		}
+		
+		// randomly pick the object and spawn it
+		float randFloat = Random.Range (0.0f, 1.0f);
+		int objIndex = 0;
+		float testValue = 0.0f;
+		
+		for(; objIndex < collectibleSpawnChanceArray.Length; ++objIndex)
+		{
+			testValue += collectibleSpawnChanceArray[objIndex];
+			
+			if(testValue >= randFloat)
+				break;
+		}
+		
+		MonoBehaviour.Instantiate (collectibleArray[objIndex], spawnPosition, Quaternion.identity);
+	}
 	
 }
