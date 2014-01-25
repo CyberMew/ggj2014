f48b076a948089103bde91b00d58104365333ab7
cybermew@gmail.com <cybermew@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-25 16:39:01 +0000

    + Menu stuffs (WIP) + Pause script (WIP)

---
+++ trunk/ProjectMorphing/Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuButton : MonoBehaviour {
+
+	public string sceneName = "MainMenu";
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseUpAsButton()
+	{
+		if(sceneName == "Exit" || sceneName == "Quit")
+		{
+			// todo: popup confirmation here
+			Application.Quit();
+		}
+		else
+		{
+			Application.LoadLevel(sceneName);
+		}
+	}
+}
---
+++ trunk/ProjectMorphing/Assets/Scripts/Pause.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour {
+
+	private float prevTimeScale;
+	// Use this for initialization
+	void Start () {
+
+		prevTimeScale = Time.timeScale;
+		Time.timeScale = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Destroy()
+	{
+		Time.timeScale = 1f;
+	}
+
+	void OnGUI()
+	{
+
+	}
+}
