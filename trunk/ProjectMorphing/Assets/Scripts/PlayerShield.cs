fe4a45a67c83ccc593fa9c2bc957712ff56adcf2
whitekero87@gmail.com <whitekero87@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-25 17:46:32 +0000

    PlayerShield ( still in work )

---
+++ trunk/ProjectMorphing/Assets/Scripts/PlayerShield.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShield : MonoBehaviour 
+{
+	private bool isShieldUp = true;
+	
+	void Start() {
+		gameObject.SetActive (isShieldUp);
+	}
+	
+	void Update() {
+		gameObject.SetActive (isShieldUp);
+	}
+
+	void OnTriggerEnter2D(Collision2D other)
+	{
+		if(isShieldUp)
+		{
+			isShieldUp = false;
+		}
+	}
+}
