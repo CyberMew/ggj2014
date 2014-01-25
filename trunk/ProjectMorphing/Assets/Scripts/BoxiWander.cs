57110043cf8de28711dab3198509375e817dc1bd
shift.phase@gmail.com <shift.phase@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-25 03:07:59 +0000

    + boxies b needin' sum lovin'

---
+++ trunk/ProjectMorphing/Assets/Scripts/BoxiWander.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxiWander : MonoBehaviour {
+	
+	public Vector3 directionVector;
+	public float speed = 0.9f;
+	Transform myTransform;
+	
+	// Use this for initialization
+	void Start () {
+		// pick a random normalized vector
+		float angleRadian = Random.Range(.0f, 2f * Mathf.PI - Mathf.Epsilon);
+		directionVector = new Vector3(Mathf.Cos(angleRadian), Mathf.Sin(angleRadian), 0f);
+		myTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// constantly move towards direction vector
+		myTransform.Translate(directionVector * Time.deltaTime * speed);
+	}
+}
