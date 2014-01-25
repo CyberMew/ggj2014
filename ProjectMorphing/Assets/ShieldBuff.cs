a1f877d89e6edb43ce0bc57e4003614237b1f61c
whitekero87@gmail.com <whitekero87@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-25 22:25:55 +0000

    Fixed tag for AggressivePlayer
    Fixed collider radius size for Shield.prefab
    
    Fix shield "regen"

---
+++ ProjectMorphing/Assets/ShieldBuff.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShieldBuff : MonoBehaviour {
+
+	private PlayerShield ps;
+	private GameObject obj;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.tag == "Player")
+		{
+			ps = other.gameObject.transform.GetComponentInChildren<PlayerShield>();
+			//obj.SetActive (true);
+			//ps = obj.GetComponent<PlayerShield>();
+			//ps = other.gameObject.transform.GetChild(1).GetComponent<PlayerShield>();//.enabled = true;
+			if(ps)
+			{
+				Debug.Log("ohyeah");
+				ps.ShieldsUp ();
+			}
+		}
+	}
+}
