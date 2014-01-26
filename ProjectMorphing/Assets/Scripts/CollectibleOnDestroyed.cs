700ab82284b9ee505aeffd5c9c59c7ca12eb15ca
shift.phase@gmail.com <shift.phase@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-26 06:27:33 +0000

    + COLLECTABLE SOUNDS!

---
+++ ProjectMorphing/Assets/Scripts/CollectibleOnDestroyed.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectibleOnDestroyed : MonoBehaviour {
+
+	public GameObject audioSource;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnDestroy()
+	{
+		Instantiate(audioSource, new Vector3(), Quaternion.identity);
+	}
+}
