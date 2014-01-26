5b7d2cec1ecd5c11091c4a6723b2f8d52e574946
qpwoeiqwe@gmail.com <qpwoeiqwe@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-26 04:12:36 +0000

    audio scripts

---
+++ trunk/ProjectMorphing/Assets/Scripts/PlaySoundScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySoundScript : MonoBehaviour {
+
+	public AudioClip sfx;
+	// Use this for initialization
+	void Start () {
+		
+		audio.PlayOneShot(sfx, 0.7F);
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
