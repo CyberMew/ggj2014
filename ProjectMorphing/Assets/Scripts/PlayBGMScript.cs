3699b162b634aa4d73381c9e0c8dc384de7af005
qpwoeiqwe@gmail.com <qpwoeiqwe@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-26 04:12:36 +0000

    audio scripts

---
+++ ProjectMorphing/Assets/Scripts/PlayBGMScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayBGMScript : MonoBehaviour {
+	public AudioClip bgm;
+	// Use this for initialization
+	void Start () {
+		audio.PlayOneShot(bgm, 0.7F);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
