a6edfb03acf552ed6c5ce00e32fb5c8a6f9df82b
treychua@gmail.com <treychua@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-26 02:22:07 +0000

    + collectible mode up

---
+++ ProjectMorphing/Assets/SpawnNewCollectibleOnDeath.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnNewCollectibleOnDeath : MonoBehaviour {
+
+	// Use this for initialization
+	void OnDestroy()
+	{
+		GameObject spawnManager = GameObject.FindGameObjectWithTag("CollectorSpawnManager");
+
+		if (spawnManager) {
+			spawnManager.GetComponent<CollectorSpawnerScript>().SpawnCollectible();
+		}
+	}
+}
