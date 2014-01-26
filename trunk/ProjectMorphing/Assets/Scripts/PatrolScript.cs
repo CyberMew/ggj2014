17699ecbffd4636941f6a0a5f2ffe3c0fc9e317f
shift.phase@gmail.com <shift.phase@gmail.com@f0748b32-99c7-6492-c967-7392b9bdbd43>
2014-01-26 03:08:00 +0000

    + patrol script is fixed

--- trunk/ProjectMorphing/Assets/Scripts/PatrolScript.cs
+++ trunk/ProjectMorphing/Assets/Scripts/PatrolScript.cs
@@ -8,16 +8,21 @@ public class PatrolScript : MonoBehaviour
 	private Vector2 quarterVec;
 	private Vector2 target1, target2;
 	private Vector2 disp;
-	private bool isFlip = false;
+	private bool isFlipX = false;
+	private bool isFlipY = false;
 	public float speed  = 2.5f;
+	private bool posX = false;
+	private bool posY = false;
 
 	void Start()
 	{
 		halfVec = .5f * MySystem.ScreenToWorldV2(MySystem.sWIDTH, MySystem.sHEIGHT);
 		quarterVec = .5f * halfVec;
 		target1 = transform.position;
-		quarterVec.x *= (transform.position.x > 0f) ? -1f : 1f;
-		quarterVec.y *= (transform.position.y > 0f) ? -1f : 1f;
+		posX = (transform.position.x > 0f) ? true : false; 
+		posY = (transform.position.y > 0f) ? true : false; 
+		quarterVec.x *= (posX) ? -1f : 1f;
+		quarterVec.y *= (posY) ? -1f : 1f;
 		target2.x = target1.x + Random.insideUnitCircle.x * quarterVec.x;
 		target2.y = target1.y + Random.insideUnitCircle.y * quarterVec.y;
 		target2 += quarterVec;
@@ -28,11 +33,51 @@ public class PatrolScript : MonoBehaviour
 
 	void FixedUpdate()
 	{
-		if(transform.position.x > target2.x)
-			isFlip = true;
-		if(transform.position.x < target1.x)
-			isFlip = false;
+		if(posX)
+		{
+			if(transform.position.x < target2.x)
+			{
+				isFlipX = true;
+			}
+			if(transform.position.x > target1.x)
+			{
+				isFlipX = false;
+			}
+		}
+		else
+		{
+			if(transform.position.x > target2.x)
+			{
+				isFlipX = true;
+			}
+			if(transform.position.x < target1.x)
+			{
+				isFlipX = false;
+			}
+		}
+		if(posY)
+		{
+			if(transform.position.y < target2.y)
+			{
+				isFlipY = true;
+			}
+			if(transform.position.y > target1.y)
+			{
+				isFlipY = false;
+			}
+		}
+		else
+		{
+			if(transform.position.y > target2.y)
+			{
+				isFlipY = true;
+			}
+			if(transform.position.y < target1.y)
+			{
+				isFlipY = false;
+			}
+		}
 
-		transform.Translate (isFlip ? -disp : disp);
+		transform.Translate(isFlipX ? -disp.x : disp.x, isFlipY ? -disp.y : disp.y, 0f);
 	}
 }
